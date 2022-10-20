98a43706df9d3de54d1b0d0073a51b849e313ead
Ivan <bel3187227@yandex.ru>
2022-10-20 07:18:04 +0300

    Написана программа Test06

--- Test06/Program.cs
+++ Test06/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 6: Напишите программу, которая на вход принимает 
+// число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.Write("Привееет! Укажи число которое хочешь проверить - ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if ((number % 2) < 1)
+{
+    Console.Write($"{number} - является четным числом");
+}
+else
+{
+    Console.WriteLine($"{number} - не является четным числом");
+}
