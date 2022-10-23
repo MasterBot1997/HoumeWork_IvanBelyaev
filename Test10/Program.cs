2bfeb127d0fbe60f172debac037f1ade81b85e1d
Ivan <bel3187227@yandex.ru>
2022-10-23 20:03:55 +0300

    Выполнено задание Test10

--- Test10/Program.cs
+++ Test10/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на 
+// вход трёхзначное число и на выходе показывает 
+// вторую цифру этого числа. Выполнить с помощью 
+// числовых операций (целочисленное деление, остаток от деления).
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+// Вариант где число задается рандомно
+int number = new Random().Next(100, 1000);
+Console.Write($"Введите трёхзначное число: {number} ");
+// Вариант где число задает пользователь
+// int number = Convert.ToInt32(Console.ReadLine());
+
+//Решение с меодом
+
+// int DelSecondDigit(int num)
+// {
+//     int number1 = num / 10;
+//     int number2 = number1 % 10;
+
+//     return lastNumber;
+// }
+// int midNumber = DelSecondDigit(number);
+// Console.WriteLine($"Число посередине = {midNumber} ");
+
+// Через простое математическое выражение
+int midNumber = number / 10 % 10;
+Console.WriteLine($"Число посередине = {midNumber} ");
