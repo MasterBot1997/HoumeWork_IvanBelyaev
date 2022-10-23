852cec8eee34d76fb35b405d4e9eb0456d08dacb
Ivan <bel3187227@yandex.ru>
2022-10-24 02:51:43 +0300

    Попробовал добавить метод в решение 13 задачи

--- Test15/Program.cs
+++ Test15/Program.cs
@@ -6,17 +6,35 @@
 // 7 -> да
 // 1 -> нет
 
+// Вариант без метода
+
 Console.Write("Введите номер дня недели: ");
 int number = Convert.ToInt32(Console.ReadLine());
-if (number == 1 ||number == 2 ||number == 3 ||number == 4 ||number == 5)
-{
-    Console.WriteLine("Нет");
-}
-else if (number == 6 || number == 7)
-{
-    Console.WriteLine("Да");
-}
-else
-{
-    Console.WriteLine("Упс! В неделе только 7 дней-_-. Другие числа не соответствуют требования");
-}
+
+if (number == 1 ||number == 2 ||number == 3 ||number == 4 ||number == 5) Console.WriteLine("Нет");
+else if (number == 6 || number == 7) Console.WriteLine("Да");
+else Console.WriteLine("Упс! В неделе только 7 дней-_-. Другие числа не соответствуют требования");
+
+
+//  Пробовал как и в 13 задаче сделать через метод, но я не понял как правильно 
+// задать метод, выдает ошибку и я смотрел на офиц сайте ее значение но не понял
+// в чем корень проблемы. 
+
+// bool DayOfWeek(int digit)
+// {
+//     if(digit==6 || digit==7) return true; 
+//     return false; 
+// }
+
+// Console.Write("Введите номер дня недели: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// bool result = DayOfWeek(digit);
+// if (result)
+// {
+//     Console.WriteLine("Yes");
+// }
+// else
+// {
+//     Console.WriteLine("No");
+// }
