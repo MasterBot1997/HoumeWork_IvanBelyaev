69dda658c644a71b3caa16309249d87a7daea37e
Ivan <bel3187227@yandex.ru>
2022-10-26 21:46:04 +0300

    Выполнил задание Test23

--- Test23/Program.cs
+++ Test23/Program.cs
@@ -1,3 +1,23 @@
 ﻿// Напишите программу, которая 
 // 1.принимает на вход число (N) и 
 // 2.выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите верхнюю границу таблицы кубов N:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void SquareTable(int num)
+{
+    if (num > 0)
+    {
+        for (int i = 1; i <= num; i++)
+        {
+            Console.WriteLine($"|{i, 2}|-->|{Math.Pow(i, 3), 4}|");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Введите значение больше 0");
+    }
+}
+
+SquareTable(n);
