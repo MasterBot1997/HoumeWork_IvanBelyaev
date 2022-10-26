eab297d77577480271e965defc529f1055bed3af
Ivan <bel3187227@yandex.ru>
2022-10-26 13:47:40 +0300

    Попробовал прописть алгоритм через цикл While с методом

--- Test15/Program.cs
+++ Test15/Program.cs
@@ -8,33 +8,44 @@
 
 // Вариант без метода
 
-Console.Write("Введите номер дня недели: ");
-int number = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите номер дня недели: ");
+// int number = Convert.ToInt32(Console.ReadLine());
 
-if (number == 1 ||number == 2 ||number == 3 ||number == 4 ||number == 5) Console.WriteLine("Нет");
-else if (number == 6 || number == 7) Console.WriteLine("Да");
-else Console.WriteLine("Упс! В неделе только 7 дней-_-. Другие числа не соответствуют требования");
+// if (number == 1 ||number == 2 ||number == 3 ||number == 4 ||number == 5) Console.WriteLine("Нет");
+// else if (number == 6 || number == 7) Console.WriteLine("Да");
+// else Console.WriteLine("Упс! В неделе только 7 дней-_-. Другие числа не соответствуют требования");
 
 
 //  Пробовал как и в 13 задаче сделать через метод, но я не понял как правильно 
 // задать метод, выдает ошибку и я смотрел на офиц сайте ее значение но не понял
 // в чем корень проблемы. 
 
-// bool DayOfWeek(int digit)
-// {
-//     if(digit==6 || digit==7) return true; 
-//     return false; 
-// }
+bool DayOfWeek(int number)
+{
+    if (number == 6 || number == 7) return true;
+    return false;
+}
 
-// Console.Write("Введите номер дня недели: ");
-// int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите номер дня недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+// if (number < 0 || number > 7)
+// {
+//     Console.WriteLine("Такого дня неели нет");
+// }
+bool result = DayOfWeek(number);
+while (false)
+{
+    if (number < 1 || number > 7)
+    {
+        number = Convert.ToInt32(Console.ReadLine());
+    }
 
-// bool result = DayOfWeek(digit);
-// if (result)
-// {
-//     Console.WriteLine("Yes");
-// }
-// else
-// {
-//     Console.WriteLine("No");
-// }
+    if (result)
+    {
+        Console.WriteLine("Yes");
+    }
+    else
+    {
+        Console.WriteLine("No");
+    }
+}
