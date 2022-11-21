4baf29824b9f73d29e304d40ce9655faa21733a6
Ivan <bel3187227@yandex.ru>
2022-11-21 19:02:01 +0300

    Добавлен текс в тест файл

--- Testing_file/Program.cs
+++ Testing_file/Program.cs
@@ -65,3 +65,28 @@
 
 // int result = Digit(n);
 // Console.WriteLine($"{result}");
+
+// Для домашки
+// double[] CreateArrayRndDouble(int size, int min, int max)
+// {
+// double[] array = new double[size];
+// Random rnd = new Random();
+// for (int i = 0; i < size; i++)
+// {
+// array[i] = rnd.NextDouble() * (max - min) + min;
+// }
+// return array;
+// }
+
+// Рандом массив дабл с округлением
+// double[] CreateArrayRndDouble(int size, int min, int max)
+// {
+// double[] array = new double[size];
+// Random rnd = new Random();
+// for (int i = 0; i < size; i++)
+// {
+// double num = rnd.NextDouble() * (max - min) + min;
+// array[i] = Math.Round(num, 1);
+// }
+// return array;
+// }
