504a19b793901be0173016eb48f8d53fc4ad2521
Ivan <bel3187227@yandex.ru>
2022-11-26 18:23:52 +0300

    Выполнено задание 41

--- Test41/Program.cs
+++ Test41/Program.cs
@@ -3,5 +3,47 @@
 
 // 0, 7, 8, -2, -2 -> 2
 // -1, -7, 567, 89, 223-> 3
-Console.Write("[");
-Console.WriteLine("]");
+
+int[] NameArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write(array[i] + ",");
+        else Console.Write(array[i]);
+    }
+    Console.Write("]");
+}
+
+int ArrayNumberMoreThenZero(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count = count + 1;
+    }
+
+    return count;
+}
+
+
+Console.WriteLine("Введите количество элементов массива М: ");
+int numM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите числа которые вы хотиите внести в массив: ");
+
+int[] nameArray = NameArray(numM);
+Console.Write("Ваш массив -> ");
+PrintArray(nameArray);
+int arrayNumberMoreThenZero = ArrayNumberMoreThenZero(nameArray);
+Console.WriteLine($" - количество значений больше 0 = {arrayNumberMoreThenZero}");
--- Testing_file/Program.cs
+++ Testing_file/Program.cs
@@ -89,4 +89,35 @@
 // array[i] = Math.Round(num, 1);
 // }
 // return array;
-// }
+// };
+
+//------------------------------------------------------------------------------------
+// Создание массива с определением размера пользоватеелем и заполнением пользователем
+
+Console.WriteLine("Введите количество элементов массива : ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int[] NameArray(int size)
+{
+ int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+       array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write(array[i] + ",");
+        else Console.Write(array[i]);
+    }
+    Console.Write("]");
+}
+
+int[] nameArray = NameArray(num);
+PrintArray(nameArray);
