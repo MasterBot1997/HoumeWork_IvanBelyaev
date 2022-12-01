73f944520c0e5e392c3f101450e7413b597ad0bd
Ivan <bel3187227@yandex.ru>
2022-12-01 19:09:12 +0300

    исправил ошибку 47 задач, не поменял matrix.GetLength() у индекса i и j из за этого код некорректно определял размер массива, поэтому работало только с квадратными матрцами

--- Test47/Program.cs
+++ Test47/Program.cs
@@ -11,9 +11,9 @@ double[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
     double[,] matrix = new double[rows, columns];
     Random rnd = new Random();
 
-    for (int i = 0; i < matrix.GetLength(1); i++)
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        for (int j = 0; j < matrix.GetLength(0); j++)
+        for (int j = 0; j < matrix.GetLength(1); j++)
         {
             double num = rnd.NextDouble() * (max - min) + min;
             matrix[i, j] = Math.Round(num, 2);
