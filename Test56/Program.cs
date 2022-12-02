a426dcb6853e2dba983d54a819fd28241107f1da
Ivan <bel3187227@yandex.ru>
2022-12-02 16:34:36 +0300

    выполнена задача 56

--- Test56/Program.cs
+++ Test56/Program.cs
@@ -11,3 +11,85 @@
 // Программа считает сумму элементов в каждой строке и выдаёт 
 // номер строки с наименьшей суммой элементов: 1 строка
 
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],3}|");
+            else Console.Write($"{matrix[i, j],3}");
+        }
+        Console.WriteLine(" |");
+    }
+}
+
+int[] SumRows(int[,] matrix)
+{
+    int[] sum = new int[matrix.GetLength(0)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum[i] = sum[i] + matrix[i, j];
+        }
+    }
+    return sum;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]},");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+void MinRows(int[] arr)
+{
+    int min = arr[0];
+    int j = 0;
+    while (j < arr.Length)
+    {
+        for (int i = 1; i < arr.Length; i++)
+        {
+            if (min > arr[i]) min = arr[i];
+        }
+        if (min == arr[j])
+        {
+            j = j + 1;
+            Console.Write($"наименьша сумма элементов в {j} строке и равны {min}");
+            break;
+        }
+        else j++;
+    }
+}
+
+int[,] array2D = CreateMatrixRndInt(8, 4, 1, 10);
+PrintMatrix(array2D);
+int[] array = SumRows(array2D);
+Console.WriteLine("==============================");
+PrintArray(array);
+Console.WriteLine("==============================");
+MinRows(array);
+
