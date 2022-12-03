8850ac88dc5a76e62b3cb328f0dfa96bff7494a4
Ivan <bel3187227@yandex.ru>
2022-12-03 15:27:44 +0300

    Добавил шаблон заполнения трехмерного массива

--- Testing_file/Program.cs
+++ Testing_file/Program.cs
@@ -310,3 +310,44 @@
 // Console.WriteLine();
 // PrintArray(array1);
 //=================================================================================
+
+//Шаблон заполнения трехзначной матрицы с прибавление каждщго следующего числа на 1
+
+// int[,,] CreateMatrix3DRndInt(int rows, int columns, int depth)
+// {
+//     int[,,] matrix = new int[rows, columns, depth];
+//     int n = 10;
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             for (int k = 0; k < matrix.GetLength(2); k++)
+//             {
+//                 matrix[i, j, k] = matrix[i, j, k] + n;
+//                 n++;
+//             }
+//         }
+//     }
+//     return matrix;
+// }
+
+// void PrintMatrix3D(int[,,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         Console.Write("");
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             for (int k = 0; k < matrix.GetLength(2); k++)
+//             {
+//                 if (k < matrix.GetLength(2) - 1) Console.Write($"{matrix[i, j, k]}({i,1},{j,1},{k,1}) ");
+//                 else Console.Write($" {matrix[i, j, k]}({i,1},{j,1},{k,1}) ");
+//             }
+//         }
+//         Console.WriteLine("");
+//     }
+// }
+
+
+//     int[,,] matrix3D = CreateMatrix3DRndInt(a, b, c);
+//     PrintMatrix3D(matrix3D);
